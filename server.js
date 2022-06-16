d1efe2b0dfefb12a3bcba935509ad3ccdacf5bf9
Stephegbenga <78284135+Stephegbenga@users.noreply.github.com>
2022-06-16 14:46:12 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,31 @@
-
 const { Client, Location, List, Buttons, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
+const express = require("express");
+
+var fs = require('fs');
+
+
+const app = express();
+
+
+const PORT = process.env.PORT || 8080;
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+// parse request to body-parser
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+
+app.use(express.json());
+
 
 const client = new Client({
     authStrategy: new LocalAuth()
@@ -18,8 +43,7 @@ client.on('authenticated', () => {
     console.log('AUTHENTICATED');
 });
 
-client.on('auth_failure', msg => {
-    // Fired if session restore was unsuccessful
+client.on('auth_failure', msg => {uccessful
     console.error('AUTHENTICATION FAILURE', msg);
 });
 
@@ -28,13 +52,29 @@ client.on('ready', () => {
 });
 
 
-client.on('message', async msg => {
-    
+app.get("/", async (req, res) => {
+    res.send("Whatsapp Api");
+});
 
-    console.log(msg);
-    if(msg.from == "120363042308165692@g.us"){
-        client.sendMessage("120363023264790007@g.us", msg.body)
-    }else if(msg.from == "120363023264790007@g.us"){
-        client.sendMessage("120363042308165692@g.us", msg.body)
-    }
-})
+
+
+app.post("/sendmessage", async (req, res) => {
+try{
+  const numbers = req.body.numbers
+  const message = req.body.message
+  console.log(numbers)
+
+  for (number of numbers){
+    console.log(number)
+      client.sendMessage(`${number}@c.us`, message)
+  }
+  res.send("Success")
+}catch(error){
+  console.log(error.message)
+}
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is up and running at ${PORT}`);
+});
